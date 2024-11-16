7ecd913061a21efa19b462501042a4a71875cf12
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-11-15 23:14:24 -0400

    add class-validator and class-transformer for input validation in createPost

--- src/posts/dto/create-post.input.ts
+++ src/posts/dto/create-post.input.ts
@@ -1,10 +1,14 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
 @InputType()
 export class CreatePostInput {
+    @MinLength(3, { message: 'Title is too short' })
+    @MaxLength(30, { message: 'Title is too long' })
+    @IsNotEmpty({ message: 'Title is required' })
     @Field()
     title: string;
-    
+
     @Field({ nullable: true })
     content?: string;
 }
