d6d47004a9733106a0c433e408cec84e39d8e237
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2024-11-15 21:01:09 -0400

    add posts module with GraphQL integration

---
+++ src/posts/posts.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { PostsService } from './posts.service';
+import { Post } from './post.entity';
+
+@Resolver()
+export class PostsResolver {
+    constructor(
+        private readonly postsService: PostsService,
+    ) {}
+
+    @Query((returns) => [Post])
+    posts() {
+        return this.postsService.findAll();
+    }
+}
